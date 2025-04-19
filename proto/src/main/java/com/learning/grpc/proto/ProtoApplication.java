1c7fc185605dfe4a947f3f163641e00bcc382f0b
shubham88fru <shubham88fru@yahoo.com>
2025-04-19 11:24:59 -0700

    initialize multi-module project

---
+++ proto/src/main/java/com/learning/grpc/proto/ProtoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.grpc.proto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoApplication.class, args);
+    }
+
+}
