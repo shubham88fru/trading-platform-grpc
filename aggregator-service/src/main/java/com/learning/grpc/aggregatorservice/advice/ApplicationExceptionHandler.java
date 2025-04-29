d4d9be54d52acf9a6ad5dcddbbd13e0563ccbbfc
shubham88fru <shubham88fru@yahoo.com>
2025-04-29 16:13:42 -0700

    global exception handler

---
+++ aggregator-service/src/main/java/com/learning/grpc/aggregatorservice/advice/ApplicationExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.learning.grpc.aggregatorservice.advice;
+
+import io.grpc.StatusRuntimeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(StatusRuntimeException.class)
+    public ResponseEntity<String> handleStatusRuntimeException(StatusRuntimeException e) {
+        return switch (e.getStatus().getCode()) {
+            case INVALID_ARGUMENT, FAILED_PRECONDITION ->
+                    ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            case NOT_FOUND -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            default -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        };
+    }
+}
